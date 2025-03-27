27f7a949aa9badf58ed9b8788848fd31927b4e09
Pierre Morin <pierremorin4590@hotmail.fr>
2025-03-27 15:37:09 +0100

    Favicon et logo de l'appli

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ let mainWindow;
 
 app.whenReady().then(() => {
   mainWindow = new BrowserWindow({
+    icon: __dirname+"/assets/copycode_logo.png",
     width: 800,
     height: 600,
     webPreferences: {
